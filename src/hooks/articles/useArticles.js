74a3633e3e35969c4409bf23d399e2177af7fd56
LarsG <lars.gunnar@jobloop.no>
2023-06-14 21:12:24 +0000

    add jsdoc to articles hook

--- src/hooks/articles/useArticles.js
+++ src/hooks/articles/useArticles.js
@@ -1,6 +1,39 @@
 import { useState, useEffect } from "react"
 import { getAllArticlesAsync } from "../../data/articles/articleHandlers"
 
+/**
+ * @description
+ * The article object returned from the realworld API
+ * 
+ * @typedef {{
+ *  slug: string;
+ *  title: string;
+ *  description: string;
+ *  body: string;
+ *  tagList: string[];
+ *  createdAt: string;
+ *  updatedAt: string;
+ *  favorited: boolean;
+ *  favoritesCount: number;
+ *  author: {
+ *      username: string;
+ *      bio: null;
+ *      image: string;
+ *      following: boolean;
+ *  };
+ *  }} Article
+ */
+
+/**
+ * @description Returns a set of variables and methods for
+ * interacting with the articles
+ * 
+ * @returns {{
+ *  isLoading: boolean
+ *  error: null | error
+ *  allArticles: null | Article[]
+ * }}
+ */
 export function useArticles() {
   const [allArticles, setAllArticles] = useState(null)
   const [error, setError] = useState(null)
