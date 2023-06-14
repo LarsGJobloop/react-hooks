1dbf42ddea78c9829aca8d2668ccbfb013606aef
LarsG <lars.gunnar@jobloop.no>
2023-06-14 21:31:51 +0000

    fix footer styling

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -2,7 +2,7 @@ import style from "./style.module.css";
 
 export function Footer() {
   return (
-    <footer className="footer">
+    <footer className={style["footer"]}>
       <h2>&copy; Lars Gunnar</h2>
     </footer>
   );
