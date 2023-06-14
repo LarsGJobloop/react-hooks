24a053d5f46736b3e1614870c27d5c046d1afc12
LarsG <lars.gunnar@jobloop.no>
2023-06-14 18:46:00 +0000

    update handlers with logging

--- src/data/articles/articleHandlers.js
+++ src/data/articles/articleHandlers.js
@@ -4,15 +4,19 @@ import articlesResponse from './articles.json'
  * Returns all the articles
  */
 export function getAllArticles() {
+  console.log("fetching all articles")
+
   return articlesResponse.articles
 }
 
 /**
  * Returns the article with matching slug
- * or undefined
+ * or undefined if none found
  * @param {string} slug
  */
 export function getArticle(slug) {
+  console.log("fetching single article")
+
   return articlesResponse.articles.find(
     (article) => article.slug === slug
   )
@@ -23,6 +27,8 @@ export function getArticle(slug) {
  * @param {string} tag
  */
 export function getArticlesWith(tag) {
+  console.log(`fetching articles with filter: ${tag}`)
+
   return articlesResponse.articles.filter(
     (article) => article.tagList.includes(tag)
   )
