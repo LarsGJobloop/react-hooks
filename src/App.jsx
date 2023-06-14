98ef8160fbd9a7e3ca7f6211bb8274697b61b1bf
LarsG <lars.gunnar@jobloop.no>
2023-06-14 17:46:46 +0000

    setup skeleton with basic css

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,47 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div className='App'>
+      <header className='navbar'>
+        <h1>Async React</h1>
+
+        <nav>
+          <ul>
+            <li>
+              <a href="#sectionLocalData">Local</a>
+            </li>
+            <li>
+              <a href="#sectionExternalData">External</a>
+            </li>
+          </ul>
+        </nav>
+      </header>
+
+      <main>
+        <section id='sectionLocalData' className='section'>
+          <header>
+            <h2>Fetching local data</h2>
+          </header>
+
+          
+        </section>
+
+        <section id='sectionExternalData' className='section'>
+          <header>
+            <h2>Fetching external data</h2>
+          </header>
+
+          
+        </section>
+      </main>
+
+      <footer className='footer'>
+        <h2>
+          &copy; Lars Gunnar
+        </h2>
+      </footer>
+    </div>
   )
 }
 
